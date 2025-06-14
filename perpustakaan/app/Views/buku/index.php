04ed81d08780fb2bccab851fc7f2f12732e2a94d
Maskha Forella <maskha.forella@gmail.com>
2025-06-15 00:41:24 +0700

    Update index.php

--- perpustakaan/app/Views/buku/index.php
+++ perpustakaan/app/Views/buku/index.php
@@ -26,6 +26,13 @@
       <div class="card-body">
         <div class="table-responsive">
 
+          <?php if (session()->getFlashdata('error')): ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+              <?= session()->getFlashdata('error') ?>
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+          <?php endif; ?>
+
           <?php if (session()->getFlashdata('success')): ?>
             <div class="alert alert-success alert-dismissible fade show" role="alert">
               <?= session()->getFlashdata('success') ?>
